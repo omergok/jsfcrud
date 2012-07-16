330890eae7895b988e8732a8ef3e54f24c7ca076
cetinimre <cetin_imre@hotmail.com>
2012-07-16 17:42:01 +0300

    asd

--- src/main/java/cetin/SampleBean.java
+++ src/main/java/cetin/SampleBean.java
@@ -3,65 +3,66 @@ package cetin;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import java.util.List;
 
-@ManagedBean(name="denemeBean")
-@ViewScoped 
-public class SampleBean {
-	
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@ManagedBean(name = "denemeBean")
+@ViewScoped
+public class SampleBean implements Serializable {
+
+	private static final long serialVersionUID = 7961263138805994351L;
+
 	private String databinding;
 	private List<User> lstUser;
 
 	public String getDatabinding() {
-		if (!FacesContext.getCurrentInstance().isPostback() ) {
-			
+		if (!FacesContext.getCurrentInstance().isPostback()) {
+
 			getUsers();
 		}
-		
+
 		return databinding;
 	}
-	
-	
-	
-	private void foo()
-	{
+
+	private void foo() {
 		System.out.println("Fooooo");
 		System.out.println("Fooo2");
-		
-		int x=3;
-	
+
+		int x = 3;
+
 	}
-	
 
 	private void getUsers() {
 		User u;
-		for (int i = 0; i < 30; i++) {
-			u=new User();
-			u.setName("name "+i+1);
-			u.setLastname("lastname "+i+1);
-			
-			if (getLstUser()!=null) {
-				getLstUser().add(u);	
-			}
+
+		if (getLstUser() == null) {
+			setLstUser(new CopyOnWriteArrayList<User>());
 		}
+
+		
+			for (int i = 0; i < 30; i++) {
+				u = new User();
+				u.setName("name " + i + 1);
+				u.setLastname("lastname " + i + 1);
+
+				getLstUser().add(u);
+
+			}
 		
 	}
-
 
 	public void setDatabinding(String databinding) {
 		this.databinding = databinding;
 	}
 
-
 	public List<User> getLstUser() {
 		return lstUser;
 	}
 
-
 	public void setLstUser(List<User> lstUser) {
 		this.lstUser = lstUser;
 	}
-	
-	
 
 }
