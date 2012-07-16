0ec64168b64293bcabed0c2cf8e8c0152353aff4
erim.erturk <erim.erturk@prime.com.tr>
2012-07-16 17:44:01 +0300

    system.out removed

--- src/main/java/tr/com/prime/erim/CalculationBean.java
+++ src/main/java/tr/com/prime/erim/CalculationBean.java
@@ -23,14 +23,8 @@ public class CalculationBean {
 	public void sum()
 	{
 
-		System.out.println("test123");
-
-
-		System.out.println("test123123");
-
 		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage("sum = " + (firstNum + secondNum)));
-		System.out.println("test321");
 	}
 
 }
